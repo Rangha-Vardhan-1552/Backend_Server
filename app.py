4a3960b484beac416927f35ae0dee00339539161
Rangha-Vardhan-1552 <113281417+Rangha-Vardhan-1552@users.noreply.github.com>
2024-04-21 15:15:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ openai.api_key = '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'
 
 # Initialize Flask application
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "http://localhost:8000"}})
+CORS(app, resources={r"/*": {"origins": "https://0690-103-120-51-73.ngrok-free.app"}})
 
 def generate_response(prompt):
     # Generate a response from the chat model
